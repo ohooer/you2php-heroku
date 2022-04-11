0f8135fd8241c3745c23b6c74cb3b62208a1757c
ohooer <57606124+ohooer@users.noreply.github.com>
2022-04-11 16:19:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyCu6UNoNwDC3N1tu5tdhiSPUClNplLcOS8');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EN2DEKEY', 'AnQUP1ADEF437e67XtzDSh2');//一个随机字符串，加解密KEY
+define('EMAIL', 'ohooer@outlook.com');//邮箱
 ?>
